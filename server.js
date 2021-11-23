1de787950cb5a1484826ef4c58917905f53fa4fd
MathiasV-Flexso <mathias.vansant@flexso.com>
2021-11-23 17:11:33 +0100

    add comment for student version

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 const { callbackify } = require('util');
 const ROUNDS_KILLER_INACTIVE = 5;
 
-app.use(express.static('public'));
-
 app.use(express.urlencoded());
 app.use(express.json());
 
@@ -18,12 +16,14 @@ app.use((req, res, next) => {
 });
 
 app.get('/data', function (req, res) {
-    axios.get("https://htf-2021.herokuapp.com/testdata.json").then((response)=>{
+    // REQUIRED
+    // Get gamedata from "https://htf-2021.herokuapp.com/testdata.json" using axios and send data to frontend
+    /*axios.get("https://htf-2021.herokuapp.com/testdata.json").then((response)=>{
         var oData = response.data;
         res.send(oData);
     }).catch((e)=>{
         console.log(`Error: ${e}`);
-    });
+    });*/
 });
 
 app.get('/new_solution', async function (req, res) {
@@ -40,98 +40,7 @@ app.get('/new_solution', async function (req, res) {
         res.send(false);
     }
 });
-
-function _clearLocalFiles(){
-    var guesses = {"guesses": []};
-    var location = {"location":[]};
-    // Player
-    fs.writeFileSync('gamedata/guesses_player.json', JSON.stringify(guesses));
-    // Bots
-    for(var i = 0; i < 4; i++){
-        fs.writeFileSync(`gamedata/guesses_bot${i+1}.json`, JSON.stringify(guesses));
-    }
-    // Killer
-    fs.writeFileSync('gamedata/killer_location.json', JSON.stringify(location));
-}
-
-async function _createSolution(){
-    var resp = await axios.get("https://htf-2021.herokuapp.com/testdata.json");
-    var oData = resp.data;
-    return {
-        "wapen": oData.wapens[_getRandomInt(oData.wapens.length)],
-        "dader": oData.daders[_getRandomInt(oData.daders.length)],
-        "kamer": oData.kamers[_getRandomInt(oData.kamers.length)]
-    }
-}
-
-async function _checkStatus(playerData, kamerId){
-    let rawdata = fs.readFileSync(`gamedata/guesses_player.json`).toString(); // Read data
-    var playerGuesses = JSON.parse(rawdata);
-    if(playerData.killerActivated && playerGuesses.guesses.length >= ROUNDS_KILLER_INACTIVE){
-        // Killer active
-        if(!playerData.killerLocation){ // Create Killer location if not existing yet
-            let killerLocation = await _createSolution();
-            playerData.killerLocation = killerLocation.kamer.id;
-        }
-        playerData.killerLocation = parseInt(playerData.killerLocation);
-        kamerId = parseInt(kamerId);
-        if(playerData.killerLocation === kamerId){
-            return false; // false == dead, true == alive
-        } else {
-            return true;
-        }
-    } else {
-        // Killer not (yet) active 
-        return true;
-    }
-}
-
-async function makeGuess(roomsUsed){
-    const tempGuess = await _createSolution();
-    if(!roomsUsed.includes(tempGuess.kamer.id)){
-        return tempGuess;
-    } else {
-        return makeGuess(roomsUsed);
-    }
-}
-
-async function _handleBotData(playerData){
-    var botGuesses = [];
-    var botLocations = [];
-    var botChecks = [];
-    let roomsUsed = [];
-    for(var i = 0; i < playerData.botStatuses.length; i++){ // For each bot
-        if(playerData.botStatuses[i]){
-            const tempGuess = await makeGuess(roomsUsed);        
-            roomsUsed.push(tempGuess.kamer.id);    
-            
-            // save valid temp guess
-            botGuesses[i] = tempGuess;
-            // save bot location
-            botLocations[i] = botGuesses[i].kamer.name;
-            // Check Bot guess
-            botChecks[i] = _checkData(botGuesses[i]);
-            // Check bot status (dead or alive)
-            playerData.botStatuses[i] = await _checkStatus(playerData, botGuesses[i].kamer.id);
-            // Write bot guess
-            _writeGuess(botGuesses[i], `gamedata/guesses_bot${i+1}.json`);
-        }  
-    }
-    return {
-        botChecks: botChecks,
-        botStatuses: playerData.botStatuses,
-        botLocations: botLocations
-    }
-}
-
-function _parseBotStatuses(statuses){
-    let parsedvalues = [];
-    statuses.forEach(element => {
-        parsedvalues.push((element === 'true'))
-    });
-    return parsedvalues;
-}
-
+    
 app.post('/check_answer', jsonParser, async (req, res) => {
     var playerData = req.body.data;
     var response;
@@ -168,21 +77,115 @@ app.post('/check_answer', jsonParser, async (req, res) => {
     }
 });
 
+function _clearLocalFiles(){
+    var guesses = {"guesses": []};
+    var location = {"location":[]};
+    // Player
+    fs.writeFileSync('gamedata/guesses_player.json', JSON.stringify(guesses));
+    // Bots
+    for(var i = 0; i < 4; i++){
+        fs.writeFileSync(`gamedata/guesses_bot${i+1}.json`, JSON.stringify(guesses));
+    }
+    // Killer
+    fs.writeFileSync('gamedata/killer_location.json', JSON.stringify(location));
+}
+
+async function _createSolution(){
+    var resp = await axios.get("https://htf-2021.herokuapp.com/testdata.json");
+    var oData = resp.data;
+    // REQUIRED
+    // Get random objects from oData by using random generator (_getRandomInt)
+    return {
+        "wapen": "", //oData.wapens[_getRandomInt(oData.wapens.length)],
+        "dader": "", //oData.daders[_getRandomInt(oData.daders.length)],
+        "kamer": "", //oData.kamers[_getRandomInt(oData.kamers.length)]
+    }
+}
+
+async function _handleBotData(playerData){
+    var botGuesses = [];
+    var botLocations = [];
+    var botChecks = [];
+    let roomsUsed = [];
+    for(var i = 0; i < playerData.botStatuses.length; i++){ // For each bot
+        if(playerData.botStatuses[i]){
+            const tempGuess = await _makeGuess(roomsUsed);        
+            roomsUsed.push(tempGuess.kamer.id);
+            
+            // save valid temp guess
+            botGuesses[i] = tempGuess;
+            // save bot location
+            botLocations[i] = botGuesses[i].kamer.name;
+            // Check Bot guess
+            botChecks[i] = _checkData(botGuesses[i]);
+            // Check bot status (dead or alive)
+            playerData.botStatuses[i] = await _checkStatus(playerData, botGuesses[i].kamer.id);
+            // Write bot guess
+            _writeGuess(botGuesses[i], `gamedata/guesses_bot${i+1}.json`);
+            // BONUS 2.0
+            // Make smart bot: bot learns from its guesses and keeps correct answers!
+        }  
+    }
+    return {
+        botChecks: botChecks,
+        botStatuses: playerData.botStatuses,
+        botLocations: botLocations
+    }
+}
+
+async function _checkStatus(playerData, kamerId){
+    let rawdata = fs.readFileSync(`gamedata/guesses_player.json`).toString(); // Read data
+    var playerGuesses = JSON.parse(rawdata);
+    if(playerData.killerActivated && playerGuesses.guesses.length >= ROUNDS_KILLER_INACTIVE){
+        // BONUS 2.0
+        // Make the killer play its own turn, then check if he is in the same room as the player. Return true if player survives, else return false.
+        // Killer active
+        /*if(!playerData.killerLocation){ // Create Killer location if not existing yet
+            let killerLocation = await _createSolution();
+            playerData.killerLocation = killerLocation.kamer.id;
+        }
+        playerData.killerLocation = parseInt(playerData.killerLocation);
+        kamerId = parseInt(kamerId);
+        if(playerData.killerLocation === kamerId){
+            return false; // false == dead, true == alive
+        } else {
+            return true;
+        }*/
+    } else {
+        // Killer not active 
+        return true;
+    }
+}
+
+async function _makeGuess(roomsUsed){
+    const tempGuess = await _createSolution();
+    if(!roomsUsed.includes(tempGuess.kamer.id)){
+        return tempGuess;
+    } else {
+        return _makeGuess(roomsUsed);
+    }
+}
+
 function _checkData(currentAnswer){
     let rawdata = fs.readFileSync('gamedata/solution.json').toString();
     var solution = JSON.parse(rawdata);
+    // REQUIRED
+    // Check object with existing (!) check functions. Return value should be true || false
     return {
-        wapen: _checkWapen(currentAnswer.wapen, solution.wapen),
-        dader: _checkDader(currentAnswer.dader, solution.dader),
-        kamer: _checkKamer(currentAnswer.kamer, solution.kamer)
+        wapen: "", //_checkWapen(currentAnswer.wapen, solution.wapen),
+        dader: "", //_checkDader(currentAnswer.dader, solution.dader),
+        kamer: "" //_checkKamer(currentAnswer.kamer, solution.kamer)
     };
 }
 
 function _writeGuess(currentAnswer, filePath){
-    let rawdata = fs.readFileSync(filePath).toString();
+    // REQUIRED
+    // Read content of file (filepath). Add current guess, then write the adjusted content to (filepath).
+    // Tip: use 'fs'
+    /*let rawdata = fs.readFileSync(filePath).toString();
     var guesses = JSON.parse(rawdata);
     guesses.guesses.push(currentAnswer);
-    fs.writeFileSync(filePath, JSON.stringify(guesses));
+    fs.writeFileSync(filePath, JSON.stringify(guesses));*/
 }
 
 function _checkWapen(caWapen, soWapen){
@@ -215,6 +218,14 @@ function _checkKamer(caKamer, soKamer){
 
 function _getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+function _parseBotStatuses(statuses){
+    let parsedvalues = [];
+    statuses.forEach(element => {
+        parsedvalues.push((element === 'true'))
+    });
+    return parsedvalues;
 }
 
 app.listen(port, () => console.log(`Listening at port ${port}`));
